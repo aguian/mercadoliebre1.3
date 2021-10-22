b927ea41e0c54d139ab3d895e3527b117afbafb1
aguian <aguian0104@gmail.com>
2021-10-21 23:46:18 -0300

    arreglando3

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 app.use(express.static('public'));
 const path = require('path');
 const port = process.env.port || 3000;
+app.listen(port,()=>{
+    console.log("server funcionando")
+ });
 /*const port = procserver.listen(port, ()=>{
    console.log("start server");
 });ess.env.PORT || 3000;*/
@@ -23,6 +26,4 @@ const port = process.env.port || 3000;
 app.get('/views/login.html', (req,res)=>{
     res.sendFile(path.join(__dirname, '/views/login.html'))
  });
- app.listen(port,()=>{
-    console.log("server funcionando")
- })
+ 
