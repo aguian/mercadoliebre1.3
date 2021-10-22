24bb223990fcb98b649e070d6c4fcfc439b7bfd6
aguian <aguian0104@gmail.com>
2021-10-21 23:34:29 -0300

    arreglando scrip

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 const path = require('path');
-const port = process.env.port || 3002;
+const port = process.env.port || 3000;
 /*const port = procserver.listen(port, ()=>{
    console.log("start server");
 });ess.env.PORT || 3000;*/
